6c29c458046546fab7307ae6a155c30774f014f6
PeterDGrimmUC <grimmpd@mail.uc.edu>
2023-03-16 13:00:54 -0700

    Fix serial code for package format

--- +EchoDecorrPkg/@SerialClass/SerialClass.m
+++ +EchoDecorrPkg/@SerialClass/SerialClass.m
@@ -66,10 +66,10 @@ classdef SerialClass
             n = 1; 
             for currPacket = 1:length(packetStartInds)-1
                 if packetLengths(currPacket) == 60 
-                    outDat(n) = obj.parseOutput(datIn(packetStartInds(currPacket)+1:packetStartInds(currPacket)+60),currTime)
+                    outDat(n) = obj.parseOutput(datIn(packetStartInds(currPacket)+1:packetStartInds(currPacket)+60),currTime);
                     n = n+1;
                 elseif packetLengths(currPacket) == 59
-                    outDat(n) = obj.parseOutputDisconnected(datIn(packetStartInds(currPacket)+1:packetStartInds(currPacket)+59),currTime)
+                    outDat(n) = obj.parseOutputDisconnected(datIn(packetStartInds(currPacket)+1:packetStartInds(currPacket)+59),currTime);
                     n = n+1;
                 end
                 currTime = currTime + seconds(packetLengths(currPacket)/byteRate);
@@ -81,10 +81,10 @@ classdef SerialClass
         
         function outs=parseOutput(obj,inputLine,tStamp)
             inputLine = convertStringsToChars(inputLine);
-            outs.t1 = numto14(inputLine(21),inputLine(22))/10;
-            outs.t2 = numto14(inputLine(23),inputLine(24))/10;
-            outs.t3 = numto14(inputLine(25),inputLine(26))/10;
-            outs.t4 = numto14(inputLine(27),inputLine(28))/10;
+            outs.t1 = EchoDecorrPkg.Utils.numto14(inputLine(21),inputLine(22))/10;
+            outs.t2 = EchoDecorrPkg.Utils.numto14(inputLine(23),inputLine(24))/10;
+            outs.t3 = EchoDecorrPkg.Utils.numto14(inputLine(25),inputLine(26))/10;
+            outs.t4 = EchoDecorrPkg.Utils.numto14(inputLine(27),inputLine(28))/10;
             if outs.t1 > 150
                 % invalid data reading, happens when probe is first plugged
                 % in for some reason
@@ -94,10 +94,10 @@ classdef SerialClass
                 outs.t4 = -1;
             end
             
-            outs.targetPower = numto14(inputLine(11),inputLine(12))/10;
-            outs.currentPower = numto14(inputLine(15),inputLine(16))/10;
-            outs.targetTemp = numto14(inputLine(39),inputLine(40))/10;
-            outs.impedance = numto14(inputLine(19),inputLine(20))/10;
+            outs.targetPower = EchoDecorrPkg.Utils.numto14(inputLine(11),inputLine(12))/10;
+            outs.currentPower = EchoDecorrPkg.Utils.numto14(inputLine(15),inputLine(16))/10;
+            outs.targetTemp = EchoDecorrPkg.Utils.numto14(inputLine(39),inputLine(40))/10;
+            outs.impedance = EchoDecorrPkg.Utils.numto14(inputLine(19),inputLine(20))/10;
             if outs.impedance == 1000
                 % open circuit
                 outs.impedance = -1;
@@ -122,9 +122,9 @@ classdef SerialClass
             outs.t2 = -1;
             outs.t3 = -1;
             outs.t4 = -1;
-            outs.targetPower = numto14(inputLine(11),inputLine(12))/10;
+            outs.targetPower = EchoDecorrPkg.Utils.numto14(inputLine(11),inputLine(12))/10;
             outs.currentPower = -1;
-            outs.targetTemp = numto14(inputLine(39),inputLine(40))/10;
+            outs.targetTemp = EchoDecorrPkg.Utils.numto14(inputLine(39),inputLine(40))/10;
             outs.impedance = -1;
             outs.time = tStamp; 
         end
